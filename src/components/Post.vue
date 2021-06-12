05718622bd07ac76436f004a17ee8906ee2ba902
Joran de Boer <johangerben@gmail.com>
2021-06-12 19:51:15 +0200

    Implemented blog project

---
+++ src/components/Post.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="card-container">
+  <v-card>
+    <div v-if="image" class="image-container">
+    <v-img max-width="300px" :src="require('@/assets/'+image+'')"></v-img>
+    </div>
+
+    <div class="text-container">
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-subtitle>{{date}}</v-card-subtitle>
+      <!-- eslint-disable-next-line-->
+      <v-card-text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae illum sit provident nostrum dolore, nulla earum velit odio autem veritatis, nisi unde excepturi! Impedit qui suscipit totam commodi nihil dolorem aliquam distinctio labore, placeat voluptatibus quaerat accusantium? Facere, id nulla aspernatur eum iusto sunt harum dignissimos officiis itaque. Dolore?</v-card-text>
+       <div v-if="code" class="code">
+        <v-card-subtitle>CSS</v-card-subtitle>
+       <v-card-text>
+        <pre>
+          <code>
+            {{code}}
+          </code>
+        </pre>
+      </v-card-text>
+       </div>
+      <v-card-actions>
+        <v-btn class="primary">Primary</v-btn>
+        <v-btn class="secondary">Secondary</v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: String,
+    date: String,
+    image: String,
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../scss/_settings';
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <h1>Kompjoeter</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, nesciunt!</p>
+    <div class="posts-container">
+      <Post date="06-09-2021" title="Regular Post Title"/>
+      <Post date="06-08-2021" title="Image Post Title" image="arrow.png"/>
+      <Post date="05-30-2021" title="Another Post Title"/>
+      <Post code="
+      pre {
+        display: flex;
+
+        code {
+            width: 100% !important;
+            border: 1px solid #222;
+            color: #a5a714;
+            display: block;
+            }
+      }"
+      date="05-29-2021" title="A Nother Post Title"/>
+      <Post date="05-22-2021" title="This Is A Post Title" image="face.png"/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Post from '@/components/Post.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Post,
   },
 };
 </script>
+
+<style lang="scss">
+@import '../scss/_settings';
+
+  .home {
+    display: flex;
+    justify-content: center;
+    padding: 32px;
+
+    .posts-container {
+      display: flex;
+      flex-direction: column;
+      box-shadow: $shadow-main-light;
+      border-radius: 2px;
+    }
+  }
+</style>
